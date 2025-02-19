Add tfRecord file loading. Fix scaling. Update configs. Clean up training scripts.

Training/python/tf/models/transformer.py
@@ -2,7 +2,6 @@ from omegaconf import OmegaConf, DictConfig
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, MultiHeadAttention, LayerNormalization, Dropout, Softmax, Activation, Normalization
 from models.embedding import FeatureEmbedding
-from utils.training import create_padding_mask
 
 class MaskedMultiHeadAttention(tf.keras.layers.Layer):
     def __init__(self, num_heads, dim_head_key, dim_head_value, dim_out):
@@ -21,10 +20,12 @@ class MaskedMultiHeadAttention(tf.keras.layers.Layer):
 
         self.dense = tf.keras.layers.Dense(dim_out)
 
+    @tf.function
     def split_heads(self, x, batch_size, head_dim):
         x = tf.reshape(x, (batch_size, -1, self.num_heads, head_dim))
         return tf.transpose(x, perm=[0, 2, 1, 3]) # (batch_size, num_heads, seq_len, depth)
 
+    @tf.function
     def masked_attention(self, q, k, v, mask=None):
         # q * k^T
         matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
@@ -55,6 +56,7 @@ class MaskedMultiHeadAttention(tf.keras.layers.Layer):
 
         return output, attention_weights
 
+    @tf.function
     def call(self, query, key, value, attention_mask, return_attention_scores=False):
         batch_size = tf.shape(query)[0]
 
@@ -104,6 +106,7 @@ class EncoderLayer(tf.keras.layers.Layer):
         self.dropout1 = Dropout(dropout_rate)
         self.dropout2 = Dropout(dropout_rate)
 
+    @tf.function
     def call(self, x, mask, training, return_attention_scores=False):
         if return_attention_scores:
             attn_output, attn_score = self.mha(query=x, value=x, key=x, attention_mask=mask, return_attention_scores=False)
@@ -147,6 +150,7 @@ class Encoder(tf.keras.layers.Layer):
 
         self.feature_embedding = FeatureEmbedding(**embedding_kwargs)
 
+    @tf.function
     def call(self, x, mask, training, return_attention_scores=False):
         x = self.feature_embedding(x)
         x = self.dropout(x, training=training)
@@ -168,8 +172,8 @@ class Transformer(tf.keras.Model):
         super().__init__()
         encoder_kwargs = OmegaConf.to_object(encoder_kwargs)
         self.use_masked_mha = encoder_kwargs["use_masked_mha"]
-        self.particle_blocks_to_drop = [i for i, feature_names in enumerate(encoder_kwargs['embedding_kwargs']['features_to_drop'].values())
-                                                     if feature_names=='all']
+        # self.particle_blocks_to_drop = [i for i, feature_names in enumerate(encoder_kwargs['embedding_kwargs']['features_to_drop'].values())
+        #                                              if feature_names=='all']
         self.global_block_id = list(feature_name_to_idx.keys()).index('global')
         self.r_cut = encoder_kwargs['embedding_kwargs'].pop('r_cut')
         if self.r_cut is not None:
@@ -178,42 +182,25 @@ class Transformer(tf.keras.Model):
         self.encoder = Encoder(feature_name_to_idx, **encoder_kwargs)
         self.decoder_dense = [Dense(n_nodes, activation=decoder_kwargs['activation']) for n_nodes in decoder_kwargs['dim_ff_layers']]
         self.output_dense = Dense(decoder_kwargs['n_outputs'], activation=None)
-        # self.output_pred = Activation('softmax', dtype='float32', name='predictions')
         self.output_pred = Softmax()
         self.output_attn = decoder_kwargs['output_attn']
-
+    
+    @tf.function 
     def call(self, inputs, training):
-
-        # pad input tensors per particle type and create corresponding mask  
-        mask = []
-        padded_inputs = []
-        for input_id, input_ in enumerate(inputs):
-            if input_id in self.particle_blocks_to_drop: continue
-            if input_id!=self.global_block_id and self.r_cut is not None:
-                input_ = tf.ragged.boolean_mask(input_, input_[:,:,self.r_indices[input_id]] < self.r_cut)
-            input_ = input_.to_tensor()
-            padded_inputs.append(input_)
-            mask.append(create_padding_mask(input_))
-        mask = tf.concat(mask, axis=1) 
+        mask = tf.concat([tf.math.reduce_any(tf.math.not_equal(input_, 0), axis=-1) for input_ in inputs], axis=1) 
 
         padding_mask = tf.math.logical_and(tf.expand_dims(mask, axis=1), tf.expand_dims(mask, axis=-1)) # [batch, seq, seq], symmetric block-diagonal
-        # padding_mask2 = tf.math.logical_and(mask[:, tf.newaxis, :], mask[:, :, tf.newaxis]) # [batch, seq, seq], symmetric block-diagonal
         if self.use_masked_mha: # invert mask, 0 -> constituent, 1 -> padding
             padding_mask = ~padding_mask
-        # padding_mask = tf.cast(padding_mask, tf.float32)
-        # padding_mask = padding_mask[:, tf.newaxis, :, :] # additional axis for head dimension 
         padding_mask = tf.expand_dims(padding_mask, axis=1) # additional axis for head dimension 
 
         # propagate through encoder
         if self.output_attn:
-            enc_output, attn_scores = self.encoder(padded_inputs, mask=padding_mask, training=training, return_attention_scores=self.output_attn)
+            enc_output, attn_scores = self.encoder(inputs, mask=padding_mask, training=training, return_attention_scores=self.output_attn)
         else:
-            enc_output = self.encoder(padded_inputs, mask=padding_mask, training=training, return_attention_scores=self.output_attn)
+            enc_output = self.encoder(inputs, mask=padding_mask, training=training, return_attention_scores=self.output_attn)
 
          # mask padded tokens before pooling 
-        # mask = tf.cast(mask, tf.float32)
-        # enc_output = tf.boolean_mask(enc_output, tf.expand_dims(mask, axis=-1))
-        # enc_output *= mask[...,  tf.newaxis]
         enc_output *= tf.expand_dims(tf.cast(mask, tf.float32), axis=-1)
         # enc_output *= tf.expand_dims(tf.cast(mask, tf.float16), axis=-1)
         
@@ -232,24 +219,26 @@ class Transformer(tf.keras.Model):
             return output
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-  def __init__(self, d_model, warmup_steps, lr_multiplier):
-    super(CustomSchedule, self).__init__()
+    def __init__(self, d_model, warmup_steps, lr_multiplier):
+        super(CustomSchedule, self).__init__()
 
-    self.lr_multiplier = lr_multiplier
-    self.d_model = d_model
-    self.warmup_steps = warmup_steps
+        self.lr_multiplier = lr_multiplier
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
 
-  def get_config(self):
-    config = {
-    'd_model': self.d_model,
-    'warmup_steps': self.warmup_steps,
-    'lr_multiplier': self.lr_multiplier
-     }
-    return config
+    @tf.function
+    def get_config(self):
+        config = {
+        'd_model': self.d_model,
+        'warmup_steps': self.warmup_steps,
+        'lr_multiplier': self.lr_multiplier
+        }
+        return config
 
-  def __call__(self, step):
-    # step = tf.cast(step, tf.float32) # needed for serialisation during model saving
-    arg1 = tf.math.rsqrt(step)
-    arg2 = step * (self.warmup_steps ** -1.5)
+    @tf.function
+    def __call__(self, step):
+        # step = tf.cast(step, tf.float32) # needed for serialisation during model saving
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
 
-    return self.lr_multiplier * tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+        return self.lr_multiplier * tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)

Training/python/tf/train.py
@@ -5,28 +5,47 @@ import hydra
 import time
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
-from sklearn.metrics import roc_auc_score
 
-import tensorflow as tf
-# from tensorflow.keras import mixed_precision
-# policy = mixed_precision.Policy('mixed_float16')
-# mixed_precision.set_global_policy(policy)
-# print('Compute dtype: %s' % policy.compute_dtype)
-# print('Variable dtype: %s' % policy.variable_dtype)
-
-from models.taco import TacoNet
-from models.transformer import Transformer, CustomSchedule
-from models.particle_net import ParticleNet
-from utils.training import compose_datasets, log_to_mlflow
-# from checkpointer.keras_callback import KerasCheckpointerCallback
-import mlflow
-mlflow.tensorflow.autolog(log_models=False) 
+def pre_import_gpu(cfg):
+    if cfg.get("gpu"):
+        gpu_config = cfg["gpu"]
+        if os.getenv("CUDA_VISIBLE_DEVICES"):
+            visible_devices = os.getenv("CUDA_VISIBLE_DEVICES")
+        else:
+            if not gpu_config == "all":
+                # Use default if all GPUs are requested
+                visible_devices = gpu_config
+            os.environ["CUDA_VISIBLE_DEVICES"] = visible_devices
+        print("Trying to use GPUs: {}".format(visible_devices))
+        return visible_devices
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        print("Trying to use no GPUs")
+        return ""
 
 @hydra.main(config_path='configs', config_name='train')
 def main(cfg: DictConfig) -> None:
-    print(cfg)
+    
+    visible_gpu = pre_import_gpu(cfg)
+    
+    import tensorflow as tf
+    # from tensorflow.keras import mixed_precision
+    # policy = mixed_precision.Policy('mixed_float16')
+    # mixed_precision.set_global_policy(policy)
+    # print('Compute dtype: %s' % policy.compute_dtype)
+    # print('Variable dtype: %s' % policy.variable_dtype)
+
+    from models.taco import TacoNet
+    from models.transformer import Transformer, CustomSchedule
+    from models.particle_net import ParticleNet
+    from utils.training import compose_datasets, log_to_mlflow
+    import mlflow
+    mlflow.tensorflow.autolog(log_models=False, log_datasets=False) 
+    # from checkpointer.keras_callback import KerasCheckpointerCallback
+    
+    
     cpus = tf.config.list_physical_devices('CPU')
-    if cpus:
+    if cpus and cfg.get("cpu"):
         cpu_config = cfg["cpu"]
         threads_per_core = 2
         n_cpus = int(cpu_config["cores"])
@@ -35,6 +54,8 @@ def main(cfg: DictConfig) -> None:
             cpu_max = 4
         else:
             cpu_max = int(os.getenv("OMP_NUM_THREADS"))
+            # Overwrite for solo TOpAS
+        cpu_max  = 255
         if n_cpus > cpu_max:
             raise Exception("More CPU cores assigned than available.")
         available_threads = n_cpus * threads_per_core
@@ -46,40 +67,32 @@ def main(cfg: DictConfig) -> None:
                 intra_threads, inter_threads, available_threads
             ))
         print("Using {} for intra op parallelism and {} for inter op parallelism.".format(intra_threads, inter_threads))
-        tf.config.threading.set_intra_op_parallelism_threads(intra_threads)
-        tf.config.threading.set_inter_op_parallelism_threads(inter_threads)
-    # setup gpu
+        # Overwrite for solo TOpAS
+        # tf.config.threading.set_intra_op_parallelism_threads(intra_threads) 
+        # tf.config.threading.set_inter_op_parallelism_threads(inter_threads)
+        tf.config.threading.set_inter_op_parallelism_threads(25)
+        tf.config.threading.set_intra_op_parallelism_threads(255)
+        tf.config.set_soft_device_placement(True)
+        tf.config.optimizer.set_jit(True)
+    else:
+        print("No cpu config set up. Leaving as is.")
 
+    # setup gpu
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
-        gpu_config = cfg["gpu"]
-        print("Use GPUs {}.".format(gpu_config['gpu_index']))
-        try:
-            use_gpus = gpu_config['gpu_index']
-            if isinstance(use_gpus, int):
-                use_gpus = [use_gpus]
-            valid_gpus = [gpu for i_gpu, gpu in enumerate(gpus) if i_gpu in use_gpus]
-            tf.config.set_visible_devices(valid_gpus, 'GPU')
-            for gpu in valid_gpus:
-                if 'gpu_mem' in gpu_config.keys():
-                    print("Set device memory limit of {} to {}GB.".format(gpu, gpu_config['gpu_mem']))
-                    tf.config.experimental.set_virtual_device_configuration(gpu,
-                       [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=gpu_config['gpu_mem']*1024)])
-                else:
-                    tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            print(e)
+        gpu_multiply = len(gpus)
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        print(len(gpus), "Physical GPUs found")
         if len(gpus) > 1:
-            use_strategy = tf.distribute.MirroredStrategy()
-            #use_strategy = tf.distribute.MultiWorkerMirroredStrategy()
+            # use_strategy = tf.distribute.MirroredStrategy()
+            use_strategy = tf.distribute.MultiWorkerMirroredStrategy()
             #use_strategy =  tf.distribute.experimental.CentralStorageStrategy()
         else:
             use_strategy = tf.distribute.get_strategy()
     else:
         use_strategy = tf.distribute.get_strategy()
-        logical_gpus = []
+        gpu_multiply = 1
         print("No GPUs found")
 
     # set up mlflow experiment id
@@ -103,12 +116,13 @@ def main(cfg: DictConfig) -> None:
         checkpoint_path = 'tmp_checkpoints'
         with use_strategy.scope():
             # load datasets 
-            train_data, val_data = compose_datasets(cfg["tf_dataset_cfg"], len(logical_gpus), input_dataset_cfg)
+            train_data, val_data = compose_datasets(cfg, gpu_multiply, input_dataset_cfg)
 
             # define model
             feature_name_to_idx = {}
             for feature_type, feature_names in input_dataset_cfg['feature_names'].items():
                 feature_name_to_idx[feature_type] = {name: i for i, name in enumerate(feature_names)}
+                
             if cfg["model"]["type"] == 'taco_net':
                 model = TacoNet(feature_name_to_idx, cfg["model"]["kwargs"]["encoder"], cfg["model"]["kwargs"]["decoder"])
             elif cfg["model"]["type"] == 'transformer':
@@ -162,11 +176,15 @@ def main(cfg: DictConfig) -> None:
                 model = ParticleNet(feature_name_to_idx, cfg['model']['kwargs']['encoder'], cfg['model']['kwargs']['decoder'])
             else:
                 raise RuntimeError('Failed to infer model type')
-            iterer = iter(train_data)
-            X_, _ = next(iterer)
-            out1 = model(X_, training=False) # init it for correct autologging with mlflow
-            # X2_, _ = next(iterer)
-            # out2 = model(X2_) # init it for correct autologging with mlflow
+            
+            # Currently hardcoded sizes
+            input_shapes = [
+                (None, None, 35),  # First feature tensor
+                (None, None, 74),  # Second feature tensor
+                (None, None, 36),  # Third feature tensor 
+                (None, 1, 43)      # Global features tensor
+            ]
+            model.build(input_shapes)
 
             # LR schedule
             if cfg['schedule'] is None: 
@@ -224,7 +242,7 @@ def main(cfg: DictConfig) -> None:
 
             path_to_hydra_logs = HydraConfig.get().run.dir
             tensorboard_logdir = f'{path_to_hydra_logs}/custom_tensorboard_logs'
-            tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_logdir, profile_batch = (10, 110))
+            tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_logdir, profile_batch = (10, 20))
 
             callbacks = [early_stopping, model_checkpoint, tensorboard_callback]
             # callbacks = [early_stopping, model_checkpoint] #, checkpointing]
@@ -235,8 +253,7 @@ def main(cfg: DictConfig) -> None:
                         loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False), 
                         metrics=['accuracy', tf.keras.metrics.AUC(from_logits=False)])
         start_time = time.time()
-        model.fit(train_data, validation_data=val_data, epochs=cfg["n_epochs"], callbacks=callbacks, verbose=2) #, steps_per_epoch=1000)
-        # model.fit(train_data, validation_data=val_data, epochs=1, callbacks=callbacks, verbose=1, steps_per_epoch=111, validation_steps=111)
+        model.fit(train_data, validation_data=val_data, epochs=cfg["n_epochs"], callbacks=callbacks, verbose=1) #, steps_per_epoch=300, validation_steps=300)
         end_time = time.time()
         print("Runtime: {}".format(end_time-start_time))
         # log info